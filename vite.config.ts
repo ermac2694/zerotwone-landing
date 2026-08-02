38b3faba900e3256c7a20eda05cb3fe20a35859d
ermac2694 <gaurav.sharma2694@gmail.com>
2026-08-03 03:14:51 +0530

    domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
-    base: '/zerotwone-landing/',
+    base: '/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
